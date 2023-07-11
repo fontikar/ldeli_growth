0111f2abc2f3f74a81681191d265d8c8349b7201
Daniel Noble <daniel.noble@anu.edu.au>
2023-07-12 08:54:11 +1000

    Adding extra models for coldsubset data

---
+++ Katana/brms_mod.12.cold.R
@@ -0,0 +1,36 @@
+#Load libraries
+library(dplyr)
+library(magrittr)
+library(brms)
+
+##Read in data
+# Dataset
+cold_DA <- read.csv("data/growth/processed/analysis/COLD_quangen_growth_DA.csv", stringsAsFactors = F)
+str(cold_DA)
+dim(cold_DA)
+
+#Format date and treatment
+cold_DA %<>% mutate(treatment = as.factor(treatment),
+                    liz_id = as.factor(liz_id),
+                    dam_id = as.factor(dam_id), 
+                    treatent = as.factor(treatment))
+
+#G matrix
+G_VCV <- read.csv("output/G/Ga_SNPready.csv", row.names = 1) %>% as.matrix()
+
+priors <- c(prior(normal(0, 10), "Intercept"),
+            prior(student_t(3, 0, 10), class = "sd"),
+            prior(student_t(3, 0, 10), class = "sigma"))
+
+#The model
+brm_12_cold <- brm(lnMass ~ 1 +
+                 (1 + z_days_since_hatch + z_days_since_hatch_I2 | gr(F1_Genotype, cov = G_VCV)) + 
+                 (1 + z_days_since_hatch + z_days_since_hatch_I2 | dam_id),
+               family = gaussian(),
+               prior = priors,
+               data2 = list(G_VCV = G_VCV),
+               data = cold_DA, 
+               chains = 4, cores = 4, iter = 6000, warmup = 1000, thin = 10,
+               control = list(adapt_delta = 0.98), save_pars = save_pars(all = TRUE))
+
+saveRDS(brm_12_cold, "output/rds/brm_12_cold")
