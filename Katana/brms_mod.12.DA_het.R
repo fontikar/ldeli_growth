d0c13f271742a2f9885230033cb03bcd2dc46817
Daniel Noble <daniel.noble@anu.edu.au>
2023-07-12 12:36:54 +1000

    Adding top model hetero

--- Katana/brms_mod.12.DA_het.R
+++ Katana/brms_mod.12.DA_het.R
@@ -19,13 +19,15 @@ G_VCV <- read.csv("output/G/Ga_SNPready.csv", row.names = 1) %>% as.matrix()
 
 # Set some prirors
 priors <- c(prior(normal(0, 10), "Intercept"),
-            prior(student_t(3, 0, 10), class = "sd"),
-            prior(student_t(3, 0, 10), class = "sigma"))
+            prior(student_t(3, 0, 10), class = "sd"))
 
 #The model
-brm_12 <- brm(lnMass ~ 1 +
-                 (1 + z_days_since_hatch + z_days_since_hatch_I2 | gr(F1_Genotype, cov = G_VCV)) + 
-                  (1 + z_days_since_hatch + z_days_since_hatch_I2 | dam_id),
+brm_12_het_mod <- 
+  bf(lnMass ~ 1 + (1 + z_days_since_hatch + z_days_since_hatch_I2 | gr(F1_Genotype, cov = G_VCV)) + 
+                  (1 + z_days_since_hatch + z_days_since_hatch_I2 | dam_id), 
+      sigma ~ z_days_since_hatch)
+
+brm_12_het <- brm(brm_12_het_mod,
                family = gaussian(),
                prior = priors,
                data2 = list(G_VCV = G_VCV),
@@ -33,4 +35,4 @@ brm_12 <- brm(lnMass ~ 1 +
                chains = 4, cores = 4, iter = 6000, warmup = 1000, thin = 10,
                control = list(adapt_delta = 0.98), save_pars = save_pars(all = TRUE))
 
-saveRDS(brm_12, "output/rds/brm_12")
+saveRDS(brm_12_het, "output/rds/brm_12_het")
