43c8d41656bd028027be30745da15251ee750197
Daniel Noble <daniel.noble@anu.edu.au>
2023-07-14 16:29:52 +1000

    Removed VonB curves

--- Katana/brms_mod.12.DA.R
+++ Katana/brms_mod.12.DA.R
@@ -36,25 +36,3 @@ brm_12 <- brm(lnMass ~ 1 +
 saveRDS(brm_12, "output/rds/brm_12")
 
 
-# We can also compare the above model to a Von Bertalanffy growth model
-growth_formula <- bf( mass ~ Linf * (1 - exp(-K * (z_days_since_hatch - t0))), 
-                      Linf + K + t0 ~ 1 + (1|gr(F1_Genotype, cov = G_VCV)) + (1|dam_id), nl = T)
-
-
-growth_prior <- c(set_prior("normal(0, 0.5)", nlpar = "Linf", lb = 0), 
-                  set_prior("normal(0,0.005)", nlpar = "K", lb = 0), 
-                  set_prior("normal(0,50)", nlpar = "t0"), 
-                  set_prior("normal(0,10)", class = "sigma"), 
-                  set_prior("normal(0,1)", class = "sd", group = "F1_Genotype", nlpar = "Linf"), 
-                  set_prior("normal(0,1e-10)", class = "sd", group = "F1_Genotype", nlpar = "K"), 
-                  set_prior("normal(0,1)", class = "sd", group = "dam_id", nlpar = "Linf"), 
-                  set_prior("normal(0,1e-10)", class = "sd", group = "dam_id", nlpar = "K")) 
-
-brm_12_Von_Bert <- brm(growth_formula, family = gaussian(), 
-               data = data_DA, 
-               data2 = list(G_VCV = G_VCV),
-               prior = growth_prior, 
-               sample_prior = T, cores = 4, chains = 4, warmup = 4000, iter = 10000, thin = 10, 
-               control = list(adapt_delta = 0.98))
-
-saveRDS(brm_12_Von_Bert, file = "output/rds/brm_12_Von_Bert")
