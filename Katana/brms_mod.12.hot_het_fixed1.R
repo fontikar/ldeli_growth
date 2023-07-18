9382ad04e4c975c886e35d09ac4bc84833c30fda
Daniel Noble <daniel.noble@anu.edu.au>
2023-07-18 11:06:07 +1000

    Adding new fixed effect models

---
+++ Katana/brms_mod.12.hot_het_fixed1.R
@@ -0,0 +1,39 @@
+#Load libraries
+library(dplyr)
+library(magrittr)
+library(brms)
+
+##Read in data
+# Dataset
+hot_DA <- read.csv("data/growth/processed/analysis/HOT_quangen_growth_DA.csv", stringsAsFactors = F)
+str(hot_DA)
+dim(hot_DA)
+
+#Format date and treatment
+hot_DA %<>% mutate(treatment = as.factor(treatment),
+                   liz_id = as.factor(liz_id),
+                   dam_id = as.factor(dam_id), 
+                   treatent = as.factor(treatment))
+#G matrix
+G_VCV <- read.csv("output/G/Ga_SNPready.csv", row.names = 1) %>% as.matrix()
+
+priors <- c(prior(normal(0, 5), "Intercept"),
+            prior(normal(0, 10), "b"),
+            prior(student_t(3, 0, 10), class = "sd"))
+
+#The model
+
+mods_hot <- bf(lnMass ~ z_days_since_hatch + z_days_since_hatch_I2 +
+                 (1 + z_days_since_hatch + z_days_since_hatch_I2 | gr(F1_Genotype, cov = G_VCV)) + 
+                 (1 + z_days_since_hatch + z_days_since_hatch_I2 | dam_id),
+                 sigma ~ z_days_since_hatch)
+
+brm_12_hot_het_fixed1 <- brm(mods_hot,
+               family = gaussian(),
+               prior = priors,
+               data2 = list(G_VCV = G_VCV),
+               data = hot_DA, 
+               chains = 4, cores = 4, iter = 6000, warmup = 1000, thin = 10,
+               control = list(adapt_delta = 0.98, max_treedepth=12), save_pars = save_pars(all = TRUE))
+
+saveRDS(brm_12_hot_het_fixed1, "output/rds/brm_12_hot_het_fixed1")
